40d7a236b48476878c52f3f24837ed23ab2daf84
fagnersro <fagner.hg40@gmail.com>
2023-02-21 01:27:30 -0300

    finalizing section 12 list project in typescript with react

--- 12_todo_react/src/components/TaskForm.tsx
+++ 12_todo_react/src/components/TaskForm.tsx
@@ -8,20 +8,66 @@ import { ITask } from '../interfaces/Task';
 
 type Props = {
   btnText: string;
+  taskList: ITask[];
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>;
+  task?: ITask | null;
+  handleUpdate?(id: number, title: string, difficulty: number): void;
 };
 
-const TaskForm = ({ btnText }: Props) => {
+const TaskForm = ({
+  btnText,
+  taskList,
+  setTaskList,
+  task,
+  handleUpdate,
+}: Props) => {
   const [id, setId] = useState<number>(0);
   const [title, setTitle] = useState<string>('');
   const [difficulty, setDifficulty] = useState<number>(0);
 
-  const addTaskHandler = () => {};
+  useEffect(() => {
+    if (task) {
+      setId(task.id);
+      setTitle(task.title);
+      setDifficulty(task.difficulty);
+    }
+  }, [task]);
+
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (handleUpdate) {
+      handleUpdate(id, title, difficulty);
+    } else {
+      const id = Math.floor(Math.random() * 1000);
+
+      const newTask: ITask = { id, title, difficulty };
+
+      setTaskList!([...taskList, newTask]);
+      setTitle('');
+      setDifficulty(0);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setDifficulty(parseInt(e.target.value));
+    }
+  };
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={addTaskHandler} className={styles.form}>
       <div className={styles.input_container}>
         <label htmlFor="title">Título:</label>
-        <input type="text" name="title" id="" placeholder="Título da tarefa" />
+        <input
+          type="text"
+          name="title"
+          id=""
+          placeholder="Título da tarefa"
+          onChange={handleChange}
+          value={title}
+        />
       </div>
       <div className={styles.input_container}>
         <label htmlFor="difficulty">Dificuldade:</label>
@@ -30,6 +76,8 @@ const TaskForm = ({ btnText }: Props) => {
           name="difficulty"
           id=""
           placeholder="Dificuldade da tarefa"
+          onChange={handleChange}
+          value={difficulty}
         />
       </div>
       <input type="submit" value={btnText} />
--- 12_todo_react/src/components/TaskList.tsx
+++ 12_todo_react/src/components/TaskList.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
-type Props = {};
+// INTERFACES
+import { ITask } from '../interfaces/Task';
 
-const TaskList = (props: Props) => {
+// CSS
+import styles from './TaskList.module.css';
+
+type Props = {
+  taskList: ITask[];
+  handleDelete(id: number): void;
+  handleEdite(task: ITask): void;
+};
+
+const TaskList = ({ taskList, handleDelete, handleEdite }: Props) => {
   return (
-    <div>
-      <p>Lista tarefas</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map(task => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i
+                className="bi bi-pencil"
+                onClick={() => {
+                  handleEdite(task);
+                }}
+              ></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => {
+                  handleDelete(task.id);
+                }}
+              ></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
   );
 };
 
