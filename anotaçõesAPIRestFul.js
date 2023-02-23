d4e3da6a9e162ef4c278b060bebab7a34891fe2e
fagnersro <fagner.hg40@gmail.com>
2023-02-22 21:00:13 -0300

    :sparkles: - Finalizing RestFull API + TS project with Data | Cloud: MongoDB

---
+++ anotaçõesAPIRestFul.js
@@ -0,0 +1,9 @@
+//  COMANDO DE INSTALAÇÕES
+/*
+  npm init -y
+  tsc --init
+  npm install config dotenv express express-validator mongoose morgan winston
+
+  dependencias de desenvolvimentos
+  
+*/
