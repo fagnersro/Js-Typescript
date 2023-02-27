17d17c6dcb927f4882ad4bfc957804b6c80d71a9
fagnersro <fagner.hg40@gmail.com>
2023-02-27 12:48:38 -0300

    Finishing Modern JavaScript section (ES6)

---
+++ 15_APENDICE_A_JSMODERNO_ES6/anotacoes.js
@@ -0,0 +1,68 @@
+// VARIÁVEIS COM LET ECONST
+/*
+Temos duas novas formas de declarar variáveis no ES6
+A let que é semelhante ao var, podemos alterar valores;
+E a const,que é uma forma de declarar constantes;
+O grande diferencial é o escopos de blocos, não temos mais variáveis
+sendo 'vazadas' para outros escopos por causa do mesmo nome;
+*/
+
+// ARROW FUNCTIONS
+/*
+A arrow function é um recurso para criar funções de forma mais simples
+mas ela não funciona exatamente como uma function em todos os aspectos
+o this da arrow function é relacionado ao elemento pai de quem está a executando;
+*/
+
+// FILTER
+/*
+O filter é um método de array para filtrar dados, baseado em alguma condição
+que estabelecemos;
+isso nos dá um array com apenas os elementos quequeremos, de forma performática;
+Há vários métodos de array importantes no ES6, este é um deles;
+*/
+
+// MAP
+/*
+O map é um método de array também, que percorre todos os elementos;
+Quando utilizamos o Map, estamos querendo modificar os dados do array;
+Há vários métodos de array importantes no ES6, este é um deles;
+*/
+
+// TEMPLATE LITERALS
+/*
+O recurso de template literais permite concatenar uma string de forma mais simples;
+Podemos colocar as variáveis entre o texto;
+A string fica entre: 'texto'
+E as variáveis são colocadas desta maneira: ${variavel}
+*/
+
+// DESTRUCTURING
+/*
+O destructuring é um recurso que pode ser utilizado em arrays e objetos;
+A ideia é transformar os itens de um desses dados em variáveis;
+Simplificando a declaração de n variáveis para apenas 1 linha
+*/
+
+//  SPREAD OPERATOR
+/*
+O Spread pode ser utilizado também em arrays e objetos;
+Utilizamos para constituir novos valores destes dados em outros arrays e objetos;
+Ou seja, podemos unir vários arrays de maneira simples ou adicionar valores de um objeto a outro, por exemplo
+*/
+
+// 8 CLASSES
+/*
+As classes são recursos fundamentais para programar com orientação a objetos;
+Agora o JS tem este recursos implementado, onde temos acesso a:
+constructor, propriedades, métodos e mais;
+Antes as classes eram baseadas em funções ( constructor functions )
+*/
+
+// 9 HERANÇA
+/*
+Podemos criar herança de classes com ES6 também;
+Utilizamos a palavra extends para referir a classe herdada;
+E as propriedades que utilizamos dela, devem ser enviadas via função super;
+Vamos ver na prática!
+*/
